d7130d163cabc3ac637aeabd442e6c652aa3e236
user <michedwards0@gmail.com>
2021-10-04 10:15:08 +0700

    Tugas Besar Aplikasi Meh Travelling

---
+++ app/src/main/java/com/example/tubespw_mehtravelling/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.tubespw_mehtravelling;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splashscreen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000L); //3000 L = 3 detik
+    }
+}
