faf13934de212750911b1eb6013aa78616328a1f
ASUS <marshandakrsna@gmail.com>
2021-10-19 21:01:48 +0700

    update 1 - ABOUT US

--- app/src/main/java/com/example/tubespw_mehtravelling/AboutUs.java
+++ app/src/main/java/com/example/tubespw_mehtravelling/AboutUs.java
@@ -1,7 +1,13 @@
 package com.example.tubespw_mehtravelling;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutUs extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+    }
 }
