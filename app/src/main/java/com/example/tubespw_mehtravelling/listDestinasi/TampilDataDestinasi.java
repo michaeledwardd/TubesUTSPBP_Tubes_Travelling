2a19d2f83452a1a253a7b610acd772c9caf5d7a5
ASUS <marshandakrsna@gmail.com>
2021-10-20 17:08:04 +0700

    selesaaiiiii, senggoll dongg

--- app/src/main/java/com/example/tubespw_mehtravelling/listDestinasi/TampilDataDestinasi.java
+++ app/src/main/java/com/example/tubespw_mehtravelling/listDestinasi/TampilDataDestinasi.java
@@ -1,6 +1,9 @@
 package com.example.tubespw_mehtravelling.listDestinasi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -8,6 +11,8 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.tubespw_mehtravelling.MainActivity;
 import com.example.tubespw_mehtravelling.databinding.ActivityTampilListDestinasiBinding;
 import com.example.tubespw_mehtravelling.R;
 
@@ -21,6 +26,7 @@ public class TampilDataDestinasi extends AppCompatActivity {
     private RecyclerView.LayoutManager mLayoutManager;
     private  ActivityTampilListDestinasiBinding binding;
     private RecyclerView rvDestinasi;
+    private Button btnbacklist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -39,6 +45,14 @@ public class TampilDataDestinasi extends AppCompatActivity {
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
         recyclerView.setAdapter(adapter);
+        btnbacklist = findViewById(R.id.btnbacklist);
+        btnbacklist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent movePage = new Intent(TampilDataDestinasi.this, MainActivity.class);
+                startActivity(movePage);
+            }
+        });
 
 
 //        binding.rvDestinasi.setLayoutManager(new LinearLayoutManager(this.getBaseContext(),LinearLayoutManager.VERTICAL,false));
